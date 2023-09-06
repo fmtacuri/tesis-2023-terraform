9e323a68b51d0deac97eab4e6703565b0ebbfdde
Freddy Tacuri <ftacurip@pichincha.com>
2023-09-06 18:53:02 -0500

    set env connection

--- main.tf
+++ main.tf
@@ -13,8 +13,10 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
+  profile    = "default"
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
   default_tags {
     tags = var.tags
   }
