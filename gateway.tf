5fe2f533ae439357efec8f4eeee300b01d16f821
Freddy Tacuri <ftacurip@pichincha.com>
2023-09-06 19:16:16 -0500

    remove ipv6

--- gateway.tf
+++ gateway.tf
@@ -40,11 +40,10 @@ resource "aws_security_group" "sg_public_instance" {
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
